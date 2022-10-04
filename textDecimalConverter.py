5d08bfc51ee36be6db2be3b2360c6e399402d78a
Poki <51330236+PokiDokika@users.noreply.github.com>
2022-10-04 20:11:22 +0300

    Added decoding and made the output nicer

--- textDecimalConverter.py
+++ textDecimalConverter.py
@@ -1,3 +1,10 @@
-text = input("Enter a String: ")
-decimal=[ord(i) for i in text]
-print(decimal) 
+action = input("Do you want to encode (e) or decode (d) (default e): ")
+
+if(action == "d"):
+    text = input("Enter numbers seperated by spaces: ")
+    result=[chr(int(i)) for i in text.split()]
+else:
+    text = input("Enter a String: ")
+    result=[str(ord(i)) for i in text]
+
+print(" ".join(result))
